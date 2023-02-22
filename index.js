72a08807ff4d126d0c608b64460d6dbe674ab817
Holden Gerner <hgerner19@gmail.com>
2023-02-22 10:55:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(active = "roller-skate"){
+    return `This Saturday, I want to ${active}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(thing = "go to the office"){
+    return `This Monday, I will ${thing}.`;
+}
+function wrapAdjective(word = "*",msg = "You are"){
+    return function(next_word = "special"){
+        return `${msg} ${word}${next_word}${word}!`;
+    };
+}
+wrapAdjective("||")("a hard worker");
+
+
